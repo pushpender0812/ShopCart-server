edf74db5a4e4d2e85987868730fb12d6cf8f2b9b
pushpender0812 <yadavpushpender6378@gmail.com>
2024-10-23 12:46:13 +0530

    mongo db Url

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/E-Commerce").then(() => {
+mongoose.connect("mongodb+srv://pyadav96800:yadav%4012@cluster0.414c6.mongodb.net/E-commerce").then(() => {
     console.log("mongo db connected successfully");
 }).catch((err) => {
     console.log(err);
